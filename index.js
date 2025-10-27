5affdf934ce6a5e2f5ad27eaa71523959ee580d4
Othallia <vianiputriocta6@gmail.com>
2025-10-27 11:54:46 +0700

    benerin typo

--- index.js
+++ index.js
@@ -11,13 +11,12 @@ app.listen(port, () => {
     console.log(`Server started on port 3000`);
 });
 
-b.sequelize.sync()
+db.sequelize.sync()
     .then((result) => {
         app.listen(3000, () => {
             console.log(`Server started`);
-        });
+        })
     })
     .catch((err) => {
-        console.error(err);
+        console.log(err);
     });
-
